821eb8ff04e659ac06811ae00773b703c840a0a7
selenahans <selenahans49@gmail.com>
2026-02-16 12:33:39 +0700

    ganti route jdi ke landing

--- cuan_buddy/routes/web.php
+++ cuan_buddy/routes/web.php
@@ -2,12 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('landing', function () {
     return view('landing');
 });
+// Route::get('landing', function () {
+//     return view('landing');
+// });
 Route::get('login', function () {
     return view('login');
 });
