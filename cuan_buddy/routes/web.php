d84f6c82486459ecf8f75a5586b3d14d9361f85a
selenahans <selenahans49@gmail.com>
2026-02-26 22:55:21 +0700

    DELETE komen di ui dashboard

--- cuan_buddy/routes/web.php
+++ cuan_buddy/routes/web.php
@@ -14,3 +14,6 @@ Route::get('notfound', function () {
 Route::get('register', function () {
     return view('register');
 });
+Route::get('dashboard', function () {
+    return view('dashboard');
+});
